2b2d0f07e7a055bf1028a3b65d965cb6cc85225e
neko <ai73a@yandex.ru>
2022-11-27 16:44:35 +0300

    add hw14

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
 // import HW13 from '../../hw13/HW13'
+// import HW14 from '../../hw14/HW14'
+// import HW15 from '../../hw15/HW15'
 
 function JuniorPlus() {
     return (
@@ -11,6 +13,8 @@ function JuniorPlus() {
             {/*<HW11 />*/}
             {/*<HW12 />*/}
             {/*<HW13 />*/}
+            {/*<HW14 />*/}
+            {/*<HW15 />*/}
         </div>
     )
 }
