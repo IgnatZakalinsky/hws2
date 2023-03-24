8d8cffd79d43bbbd34bcd2537293bef292f42597
Matthewdeveloper1 <92750111+Matthewdeveloper1@users.noreply.github.com>
2023-03-24 17:27:01 +0300

    hw2er

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -45,7 +45,9 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
-        setName(e.currentTarget.value) // need to fix
+        setName(e.currentTarget.value)
+        error && setError('')
+        // need to fix
 
         error && setError('')
     }
