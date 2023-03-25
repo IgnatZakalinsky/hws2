1c882d8abf94b5918ca396bc17ebd0dcfadf3a3e
Matthewdeveloper1 <92750111+Matthewdeveloper1@users.noreply.github.com>
2023-03-25 17:08:31 +0300

    fix error class

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -25,7 +25,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
+    const inputClass = error ? s.errorInput : '' // need to fix with (?:)
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
