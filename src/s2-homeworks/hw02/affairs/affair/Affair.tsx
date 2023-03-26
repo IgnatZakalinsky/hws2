5d32acd1ae394cac761246615b612ac8689148b9
kkapchin <kkapchin@yahoo.com>
2023-03-25 22:24:04 -0400

    в файле Affair.tsx отобразить приходящие данные

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -25,7 +25,7 @@ function Affair ({affair, deleteAffairCallback}: AffairPropsType) {
         >
             <div id={'hw2-name-' + affair._id} className={nameClass}>
                 {/*создаёт студент*/}
-
+                {affair.name}
                 {/**/}
             </div>
             <div id={'hw2-priority-' + affair._id} hidden>
