c53e914bf5020da5636e2de29bb28a94395dfa4c
kkapchin <kkapchin@yahoo.com>
2023-03-30 02:15:05 -0400

    в файле GreetingContainer.tsx вычислить количество добавленных и имя последнего (totalUsers, lastUserName)

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -64,8 +64,8 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
         pureOnEnter(e, addUser)
     }
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+    const totalUsers = users.length // need to fix
+    const lastUserName = users.length === 0 ? '' : users[users.length - 1].name; // need to fix
 
     return (
         <Greeting
