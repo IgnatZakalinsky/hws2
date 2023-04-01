c5c08faf8afac8c8de9f0fa977b17e0f29e6e363
Matthewdeveloper1 <92750111+Matthewdeveloper1@users.noreply.github.com>
2023-04-01 12:53:49 +0300

    hws5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -5,28 +5,57 @@ import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
 
+
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     JUNIOR: '/junior',
     JUNIOR_PLUS: '/junior-plus',
+    Error404: '*'
 }
+
+export const PATH1: {pathName: string, element: React.ReactElement}[] = [
+    {
+        pathName: '/',
+        element: <></>
+    },
+    {
+        pathName: '/pre-junior',
+        element: <PreJunior/>
+    },
+    {
+        pathName: '/junior',
+        element: <Junior/>
+    },
+    {
+        pathName: '/junior-plus',
+        element: <JuniorPlus/>
+    },
+    {
+        pathName: '/*',
+        element: <Error404/>
+    }
+]
 
 function Pages() {
     return (
         <div>
-            {/*Routes выбирает первый подходящий роут*/}
             <Routes>
+                {
+                    PATH1.map((item, index) => 
+                        <Route path={item.pathName} element={item.element} key={index}/>
+                    )
+                }
+            
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*<Route ...*/}
+                {/* <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR} />} />
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
+                <Route path={PATH.JUNIOR} element={<Junior />} />
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
+                <Route path={'/*'} element={<Error404 />} /> */}
             </Routes>
         </div>
     )
--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive})=> isActive? 'active' : s.NavLink } // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +39,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                         className={({isActive})=> isActive? 'active' : s.NavLink } // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +47,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive})=> isActive? 'active' : s.NavLink } // делает студент
                     >
                         Junior Plus
                     </NavLink>
