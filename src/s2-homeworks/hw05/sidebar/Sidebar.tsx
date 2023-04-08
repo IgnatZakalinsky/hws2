1603ebd8b69700298c38d6ff8421cdc68b9438d8
Matthewdeveloper1 <92750111+Matthewdeveloper1@users.noreply.github.com>
2023-04-08 19:14:20 +0300

    sidebaruse

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={({isActive})=> isActive? 'active' : s.NavLink } // делает студент
+                        className={({isActive})=> isActive? s.active : ''} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +39,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                         className={({isActive})=> isActive? 'active' : s.NavLink } // делает студент
+                         className={({isActive})=> isActive? s.active : '' } // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +47,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={({isActive})=> isActive? 'active' : s.NavLink } // делает студент
+                        className={({isActive})=> isActive? s.active : '' } // делает студент
                     >
                         Junior Plus
                     </NavLink>
