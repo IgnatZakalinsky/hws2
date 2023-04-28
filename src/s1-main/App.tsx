4bc50fc11912f40b39666a4a04a55d27bc248bd6
kkapchin <kkapchin@yahoo.com>
2023-04-28 10:51:24 -0400

    init

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './App.module.css';
-import HW4 from "../s2-homeworks/hw04/HW4";
+import HW5 from "../s2-homeworks/hw05/HW5";
 
 function App() {
     return (
@@ -9,10 +9,10 @@ function App() {
             {/*раскомментировать по ходу выполнения*/}
             {/*<HW2 />*/}
             {/*<HW3/>*/}
-            <HW4/>
+            {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            {/*<HW5/>*/}
+            <HW5/>
         </div>
     )
 }
