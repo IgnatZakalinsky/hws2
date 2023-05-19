a93206e6bc9d5ed50f3aac3ac45388ac5e4f2d6c
Anthony Shaga <antonyshaga@gmail.com>
2023-05-19 22:17:30 +0300

    hws2

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,11 +18,11 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+         + (disabled
+                 ? ` ${s.disabled}`
+                 : xType === 'red'
+                     ? ` ${s.red}`
+        + (className ? ' ' + className : ''): ` ${s.secondary}`) // задачка на смешивание классов
 
     return (
         <button
--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -31,7 +31,9 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
         if (onChangeChecked) {
             onChangeChecked(e.currentTarget.checked)
         }
-
+        if (onChange) {
+            onChange(e)
+        }
     }
 
     const finalInputClassName = s.checkbox
