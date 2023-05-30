63ec92886d7415b88baf220749fadd58ca4954e2
Anthony Shaga <antonyshaga@gmail.com>
2023-05-30 23:36:51 +0300

    hws2

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,9 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        console.log(value)
+        restoreState<string>('hw6-editable-span-value',value)
+        setValue(restoreState<string>('hw6-editable-span-value',value))
     }
 
     return (
