3de35721bcab9184dfbbe23b6484df85ce915e96
Anthony Shaga <antonyshaga@gmail.com>
2023-05-30 22:50:05 +0300

    hws2

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import HW6 from '../../hw06/HW6'
+import HW6 from "../../hw06/HW6";
 // import HW7 from '../../hw07/HW7'
 // import HW8 from '../../hw08/HW8'
 // import HW9 from '../../hw09/HW9'
@@ -7,7 +7,7 @@ import React from 'react'
 function Junior() {
     return (
         <div id={'hw5-page-junior'}>
-            {/*<HW6 />*/}
+            <HW6 />
             {/*<HW7 />*/}
             {/*<HW8 />*/}
             {/*<HW9 />*/}
