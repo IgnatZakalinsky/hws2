3f34238ec2b471b25424706ebea71ade8be7119a
Anthony Shaga <antonyshaga@gmail.com>
2023-06-12 22:47:35 +0300

    hws2

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -5,19 +5,21 @@ type ActionType =
     | { type: 'check'; payload: number }
 
 export const homeWorkReducer = (state: Array<UserType>, action: ActionType): any => { // need to fix any
+    let newState = [...state]
     switch (action.type) {
         case 'sort': { // by name
+
             if (action.payload === 'up') {
-                state.sort((a,b) => a.name > b.name ? 1 : -1)
+                newState.sort((a,b) => a.name > b.name ? 1 : -1)
             } else if (action.payload === 'down') {
-                state.sort((a,b) => a.name < b.name ? 1 : -1)
+                newState.sort((a,b) => a.name > b.name ? -1 : 1)
             }
 
-            return state // need to fix
+            return newState // need to fix
         }
         case 'check': {
-            const rest = state.filter(a=> a.age > action.payload)
-            return rest // need to fix
+            newState = state.filter(a=> a.age > action.payload)
+            return newState // need to fix
         }
         default:
             return state
