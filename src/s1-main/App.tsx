dc1a0bbc54bbd27ce93bdb998597dfd5747fc83d
Denis Pochinskiy <ircxenon1@gmail.com>
2023-06-21 23:15:21 +0500

    start hw05

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -4,18 +4,19 @@ import HW1 from '../s2-homeworks/hw01/HW1'
 import HW2 from "../s2-homeworks/hw02/HW2";
 import HW3 from "../s2-homeworks/hw03/HW3";
 import HW4 from "../s2-homeworks/hw04/HW4";
+import HW5 from "../s2-homeworks/hw05/HW5";
 
 function App() {
     return (
         <div className={s.App}>
-            <HW1/>
-            {/*раскомментировать по ходу выполнения*/}
-            <HW2/>
-            <HW3/>
-            <HW4/>
+            {/*<HW1/>*/}
+            {/*/!*раскомментировать по ходу выполнения*!/*/}
+            {/*<HW2/>*/}
+            {/*<HW3/>*/}
+            {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            {/*<HW5/>*/}
+            <HW5/>
         </div>
     )
 }
