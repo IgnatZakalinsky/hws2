42d7d4086f6db6531e4b1041ac4ba4e007ba6ffb
Anthony Shaga <antonyshaga@gmail.com>
2023-06-27 11:45:37 +0300

    hws2

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,7 +33,7 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = `${date.toLocaleTimeString('it-IT',{ hour: '2-digit', minute: '2-digit' })}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime = `${date.toLocaleTimeString('ru-Ru')}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = `${date.toLocaleDateString('ru-Ru')}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
