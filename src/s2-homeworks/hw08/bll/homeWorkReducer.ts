ab726d6388c063bc7fa32ba74fdd100fb4bf3a0f
Petr <zhirkovpetr95@gmail.com>
2023-11-10 11:30:08 +0300

    refactor: code refactor in HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,19 +7,19 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
   switch (action.type) {
     case 'sort': { // by name
-      if (action.payload === 'up') {
-       return [...state.sort((a, b) => a.name.localeCompare(b.name))]
+      switch (action.payload) {
+        case 'up': {
+          return [...state.sort((a, b) => a.name.localeCompare(b.name))]
+        }
+        case 'down': {
+          return [...state.sort((a, b) => b.name.localeCompare(a.name))]
+        }
+        default:
+          return state
       }
-      if (action.payload === 'down') {
-        return [...state.sort((a, b) => b.name.localeCompare(a.name))]
-      }
-      return state
     }
     case 'check': {
-      if (action.payload === 18) {
-        return state.filter(p => p.age >= action.payload);
-      }
-      return state // need to fix
+      return state.filter(p => p.age >= action.payload);
     }
     default:
       return state
